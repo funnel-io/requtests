29d41dc29fd974ac728eb6bc0b34fac1a8249601
Linus Huzell <linus.huzell@funnel.io>
2022-03-14 14:59:45 +0100

    Add code an tests

--- src/requtests/__init__.py
+++ src/requtests/__init__.py
@@ -1,3 +1,15 @@
+from .requtests import (
+    FakeAdapter,
+    fake_request,
+    fake_request_with_response,
+    fake_response,
+)
+
 VERSION = "1.0.0"
 
-__all__ = []
+__all__ = [
+    FakeAdapter,
+    fake_request,
+    fake_request_with_response,
+    fake_response,
+]
